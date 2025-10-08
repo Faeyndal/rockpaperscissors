b9428888860b6ea367c6d863ca8bcf1217b2bd2b
Faeyndal <5844057+Faeyndal@users.noreply.github.com>
2025-10-08 19:20:09 -0400

    Added score keeping variables

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -4,6 +4,10 @@
 let computerChoice = "blank";
 //Create a variable for the player
 let humanChoice = "blank";
+//Create variables for score keeping
+let humanScore = 0;
+let computerScore = 0;
+
 //Function for the computer's choice of option
 function getComputerChoice () {
     //Create a variable to store a random number between 1-3
@@ -30,9 +34,11 @@ function getHumanChoice () {
     return humanChoice;
 }
 
+
+
+
 getComputerChoice();
 getHumanChoice();
-
 
 console.log(humanChoice);
 console.log(computerChoice);
