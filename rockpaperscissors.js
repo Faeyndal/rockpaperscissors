0fddeb1189683e4f1f4f2fb5ab22886b65aadcc6
Faeyndal <5844057+Faeyndal@users.noreply.github.com>
2025-10-08 19:11:45 -0400

    Added code for the computer's choice

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,1 +1,26 @@
-console.log("Hello, World");
+// Rock Paper Scissors game
+
+//Create a variable for the computer to play the game
+let computerChoice = "blank";
+//Function for the computer's choice of option
+function getComputerChoice () {
+    //Create a variable to store a random number between 1-3
+    let randomChoiceNumber = (Math.floor(Math.random() * 3) + 1);
+    
+    //Change the number to a rock/paper/scissors option
+    if (randomChoiceNumber === 1) {
+        computerChoice = "rock";
+    } else if (randomChoiceNumber === 2) {
+        computerChoice = "paper";
+    } else if (randomChoiceNumber === 3) {
+        computerChoice = "scissors";
+    } else {
+        computerChoice = "error";
+    }
+    
+    return computerChoice;
+}
+
+getComputerChoice();
+
+console.log(computerChoice);
