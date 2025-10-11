017dd0074f9ebb0c99eb25d055f8f26eded5767c
Faeyndal <5844057+Faeyndal@users.noreply.github.com>
2025-10-11 18:22:43 -0400

    Added playRound to play a round of the game

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -36,10 +36,28 @@ function getHumanChoice () {
 
 //function for each round of the game
 function playRound (humanChoice, computerChoice) {
-    //check who wins the round
-
-
-    //Increase the winner's score
+        //check who wins the round
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("You lose! Paper beats Rock!");
+        computerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("You lose! Scissors beats Paper!");
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("You lose! Rock beats Scissors!");
+        computerScore++;
+    } else if (computerChoice === "rock" && humanChoice === "paper") {
+        console.log("You win! Paper beats Rock!");
+        humanScore++;
+    } else if (computerChoice === "paper" && humanChoice === "scissors") {
+        console.log("You win! Scissors beats Paper!");
+        humanScore++;
+    } else if (computerChoice === "scissors" && humanChoice === "rock") {
+        console.log("You win! Rock beats Scissors!");
+        humanScore++;
+    } else {
+        console.log("It's a tie!");
+    }
 }
 
 
